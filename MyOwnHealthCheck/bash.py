0bf35665b4afcc171d831f24fade1c37fedbd858
Gustavo Neri <guganeriss@gmail.com>
2024-09-09 17:52:55 +0100

    removendo comentarios desnecessarios

--- MyOwnHealthCheck/bash.py
+++ MyOwnHealthCheck/bash.py
@@ -7,36 +7,30 @@ from logging.handlers import RotatingFileHandler
 import os
 import time
 
-# Configurações do arquivo de log
 log_file = 'log_status.json'
 
-# Função para carregar logs existentes ou criar um novo esqueleto
 def carregar_ou_criar_arquivo_log():
     if os.path.exists(log_file):
         with open(log_file, 'r', encoding='utf-8') as file:
             try:
-                return json.load(file)  # Carrega os logs existentes
+                return json.load(file)
             except json.JSONDecodeError:
-                return {"logs": []}  # Se o arquivo estiver corrompido, recria a estrutura
+                return {"logs": []}
     else:
-        return {"logs": []}  # Cria a estrutura inicial de logs
+        return {"logs": []} 
 
-# Função para salvar logs no arquivo JSON
 def salvar_logs(logs):
     with open(log_file, 'w', encoding='utf-8') as file:
         json.dump(logs, file, ensure_ascii=False, indent=4)
 
-# Função para executar o comando bash e registrar o resultado
 def executar_comando_bash(comando):
     """Executa um comando bash e registra a saída e status."""
     try:
-        # Executa o comando Bash
         result = subprocess.run(comando, shell=True, capture_output=True, text=True)
         status_code = result.returncode
         saida = result.stdout
         erro = result.stderr
 
-        # Cria a entrada de log
         log_entry = {
             "timestamp": datetime.datetime.now().isoformat(),
             "comando": comando,
@@ -54,10 +48,8 @@ def executar_comando_bash(comando):
         return log_entry
 
     except Exception as e:
-        # Captura o traceback completo em caso de erro
         tb = traceback.format_exc()
 
-        # Logar erro com traceback completo
         log_entry = {
             "timestamp": datetime.datetime.now().isoformat(),
             "comando": comando,
@@ -75,15 +67,10 @@ comando = "curl google.com"
 
 # Loop para executar o comando a cada 30 minutos (ou ajustar conforme necessidade)
 while True:
-    # Carregar o arquivo de log ou criar um novo esqueleto
     logs = carregar_ou_criar_arquivo_log()
-
-    # Executar o comando Bash e adicionar o resultado no log
     log_entry = executar_comando_bash(comando)
     logs["logs"].append(log_entry)
 
-    # Salvar o log atualizado no arquivo
     salvar_logs(logs)
 
-    # Espera 30 minutos para a próxima execução
     time.sleep(1800)
